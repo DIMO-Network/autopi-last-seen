a6c898f3b6a89369498c7bd7ee0d562a92ac6584
Dylan Moreland <dylan@dimo.zone>
2024-10-21 09:13:57 -0400

    Prod values

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ import eth_account.messages
 
 MFR_DEVICE_SEEN_PRIV = 6
 
-# These are dev numbers.
-CLIENT_ID = "0x27950d45E810f34EA435B2da311e9e8306465Be0"
-REDIRECT_URI = "https://elffjs.org"
+CLIENT_ID = "0x97ECA5840C33Cd3563a3d2Ea0DA2d10259b113c9" # AutoPi developer license, token id 5.
+REDIRECT_URI = "https://dimo.qa.autopi.io/#/fleetvehicles"
 SIGNER_PRIVATE_KEY = os.environ["SIGNER_PRIVATE_KEY"]
-MFR_NFT_ADDR = "0xA4ad0F9c722588910791A9BAC63ADbB365614Bc7"
-MFR_TOKEN_ID = 146
-DEVICE_TOKEN_ID = 48
+MFR_NFT_ADDR = "0x3b07e2A2ABdd0A9B8F7878bdE6487c502164B9dd" # Prod address.
+MFR_TOKEN_ID = 137 # AutoPi.
+DEVICE_TOKEN_ID = 2080 # Malte's prod device.
 
 w3 = web3.Web3()
 
 # Ask for the challenge to sign. Note that we are logging in "as" (the address field)
 # the developer license address (also known as the developer license client id).
 resp = requests.post(
-    "https://auth.dev.dimo.zone/auth/web3/generate_challenge",
+    "https://auth.dimo.zone/auth/web3/generate_challenge",
     params={
         "client_id": CLIENT_ID,
         "domain": REDIRECT_URI,
@@ -42,7 +41,7 @@ signature = w3.eth.account.sign_message(
 
 # Send the challenge back.
 resp = requests.post(
-    "https://auth.dev.dimo.zone/auth/web3/submit_challenge",
+    "https://auth.dimo.zone/auth/web3/submit_challenge",
     data={
         "client_id": CLIENT_ID,
         "domain": REDIRECT_URI,
@@ -59,7 +58,7 @@ access_token = resp.json()["access_token"]
 # on the manufacturer. This must have been previously been granted to the developer
 # license address.
 resp = requests.post(
-    "https://token-exchange-api.dev.dimo.zone/v1/tokens/exchange",
+    "https://token-exchange-api.dimo.zone/v1/tokens/exchange",
     json={
         "nftContractAddress": MFR_NFT_ADDR,
         "tokenId": MFR_TOKEN_ID,
@@ -82,7 +81,7 @@ query DeviceLastSeen($tokenId: Int!) {
 """
 
 resp = requests.post(
-    "https://telemetry-api.dev.dimo.zone/query",
+    "https://telemetry-api.dimo.zone/query",
     json={"query": query, "variables": {"tokenId": DEVICE_TOKEN_ID}},
     headers={"Authorization": "Bearer " + priv_token},
 )
